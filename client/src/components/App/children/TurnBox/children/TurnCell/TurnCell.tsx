ce7b8257d929adb0eeecf9ca96a16d96d8e65481
Jackson Furr <jackson.a.furr@gmail.com>
2024-05-15 17:43:01 -0400

    ritual toggle textbox

--- client/src/components/App/children/TurnBox/children/TurnCell/TurnCell.tsx
+++ client/src/components/App/children/TurnBox/children/TurnCell/TurnCell.tsx
@@ -4,6 +4,7 @@ import { T_CATEGORY } from "../../../../../../types";
 import * as methods from "./methods";
 import { handleInput } from "../../../../../../utils/HandleInputs";
 import CATEGORY_INFO from "../../../../../../CATEGORY_INFO";
+import LevelRitualToggle from "./children/LevelRitualToggle/LevelRitualToggle";
 
 export interface IProps {
 	category: T_CATEGORY;
@@ -13,10 +14,15 @@ const TurnCell: React.FC<IProps> = (props) => {
 	const [inputValue, setInputValue] = useState<string>("");
 	const [recommendations, setRecommendations] = useState<string[]>([]);
 
-	// Handle unique category items
-	switch (props.category) {
-		case CATEGORY_INFO.LEVEL:
-			break;
+	function getUniqueItems(props: IProps): JSX.Element {
+		// Handle unique category items
+		switch (props.category) {
+			case CATEGORY_INFO.LEVEL:
+				return <LevelRitualToggle />;
+				break;
+			default:
+				return <></>;
+		}
 	}
 
 	return (
@@ -58,6 +64,7 @@ const TurnCell: React.FC<IProps> = (props) => {
 					})}
 				</div>
 			)}
+			{getUniqueItems(props)}
 		</div>
 	);
 };
