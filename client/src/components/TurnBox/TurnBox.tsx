06081c30f3a50e9e79eeb00dd35d1247bb70a0be
Jackson Furr <jackson.a.furr@gmail.com>
2024-05-19 17:50:54 -0400

    wip

--- client/src/components/TurnBox/TurnBox.tsx
+++ client/src/components/TurnBox/TurnBox.tsx
@@ -1,8 +1,15 @@
 import TurnCell from "./children/TurnCell/TurnCell";
 import styles from "./TurnBox.module.scss";
 import CATEGORY_INFO from "../../CATEGORY_INFO";
+import { useState } from "react";
+import { T_SPELL_CATEGORY_INFO, NEW_SPELL_CATEGORY_INFO } from "../../types";
+import * as methods from "../../utils/methods";
 
 const TurnBox: React.FC = () => {
+	const [guessInfo, setGuessInfo] = useState<T_SPELL_CATEGORY_INFO>(
+		methods.createNewObject(NEW_SPELL_CATEGORY_INFO)
+	);
+
 	return (
 		<div className={styles.root}>
 			<TurnCell category={CATEGORY_INFO.SCHOOL} />
