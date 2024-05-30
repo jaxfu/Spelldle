6fc6924ed42d94146bb13df56f0dbe99018ae4f3
Jackson Furr <jackson.a.furr@gmail.com>
2024-05-30 15:35:38 -0400

    refactor auth and storage to methods.tsx

--- client/src/auth/protectPage.ts
+++
@@ -1,7 +0,0 @@
-import {NavigateFunction} from "react-router-dom";
-
-function protectPage(validationCompleted: boolean, loggedIn: boolean, navigate: NavigateFunction): void {
-    if (validationCompleted && !loggedIn) navigate("/login")
-}
-
-export default protectPage;
--- client/src/utils/methods.tsx
+++ client/src/utils/methods.tsx
@@ -1,5 +1,13 @@
 import CATEGORY_INFO from "../CATEGORY_INFO";
-import { T_SINGLE_CATEGORY_POSSIBILITIES, T_SPELL_INFO } from "../types";
+import {
+	T_SINGLE_CATEGORY_POSSIBILITIES,
+	T_SPELL_INFO,
+	T_UserSessionData,
+	initUserSessionData,
+	T_ValidateResult,
+	initValidateResult,
+} from "../types";
+import { validateSession } from "./requests";
 import TextInput from "../components/TurnBox/children/TurnCell/children/TextInput/TextInput";
 import LevelRitualToggle from "../components/TurnBox/children/TurnCell/children/LevelRitualToggle/LevelRitualToggle";
 import ComponentsSelection from "../components/TurnBox/children/TurnCell/children/ComponentsSelection/ComponentsSelection";
@@ -129,4 +137,46 @@ export function onRemoveGuessClick(
 			return g.toLocaleLowerCase() !== guess.toLocaleLowerCase();
 		});
 	});
+}
+
+// Storage
+export function checkForSessionInfo(): boolean {
+	return !(
+		localStorage.getItem("session_key") === null ||
+		localStorage.getItem("session_key") === ""
+	);
+}
+
+export function getSessionInfoFromStorage(): T_UserSessionData {
+	const sessionInfo: T_UserSessionData = { ...initUserSessionData };
+
+	try {
+		sessionInfo.user_id = parseInt(localStorage.getItem("user_id") || "-1");
+		sessionInfo.session_key = localStorage.getItem("session_key") || "";
+		return sessionInfo;
+	} catch (err: any) {
+		throw new Error(err);
+	}
+}
+
+export function sendToLocalStorage(sessionInfo: T_UserSessionData) {
+	localStorage.setItem("user_id", String(sessionInfo.user_id));
+	localStorage.setItem("session_key", sessionInfo.session_key);
+
+	console.log("Sent to localStorage: ");
+	console.log(`user_id: ${localStorage.getItem("user_id")}`);
+	console.log(`session_key: ${localStorage.getItem("session_key")}`);
+}
+
+// Auth
+export async function checkValidSession(): Promise<T_ValidateResult> {
+	const invalidResult: T_ValidateResult = { ...initValidateResult };
+
+	if (!checkForSessionInfo()) return invalidResult;
+
+	try {
+		return await validateSession(getSessionInfoFromStorage());
+	} catch (err: any) {
+		throw new Error(err);
+	}
 }
