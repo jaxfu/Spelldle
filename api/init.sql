474be8ffe853dbf1314f6caa56c2d0e344fe2209
Jackson Furr <jackson.a.furr@gmail.com>
2024-05-31 21:24:29 -0400

    update SQL schemas

--- api/init.sql
+++ api/init.sql
@@ -1,31 +1,34 @@
 BEGIN;
 
-DROP TABLE IF EXISTS user_session_data;
-DROP TABLE IF EXISTS user_account_info;
+DROP TABLE IF EXISTS user_data_account;
+DROP TABLE IF EXISTS user_data_personal;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE user_account_info
+CREATE TABLE users
 (
-    user_id    SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE user_data_account
+(
+    user_id    INTEGER PRIMARY KEY UNIQUE REFERENCES users(user_id),
     username   VARCHAR(32) UNIQUE,
-    password   VARCHAR(32),
+    password   VARCHAR(32)
+);
+
+CREATE TABLE user_data_personal
+(
+    user_id    INTEGER PRIMARY KEY UNIQUE REFERENCES users(user_id),
     first_name VARCHAR(32),
     last_name  VARCHAR(32)
 );
 
-CREATE TABLE user_session_data
-(
-    user_id     INTEGER PRIMARY KEY,
-    session_key VARCHAR(36),
-    expires     BIGINT,
-    CONSTRAINT fk_user_id
-        FOREIGN KEY (user_id)
-            REFERENCES user_account_info (user_id)
-);
+INSERT INTO users DEFAULT VALUES ;
 
-INSERT INTO user_account_info (username, password, first_name, last_name)
-VALUES ('Poemmys', 'password', 'Jackson', 'Furr');
+INSERT INTO user_data_account (user_id, username, password)
+VALUES (1, 'poemmys', 'pass');
 
-INSERT INTO user_session_data (user_id, session_key, expires)
-VALUES (1, 'TEST_UUID', 1706245738);
+INSERT INTO user_data_personal(user_id, first_name, last_name)
+VALUES (1, 'Jackson', 'Furr');
 
 COMMIT;
