6b8b355014dbeb13c47369ee083fb1e9c48645e6
Jackson Furr <jackson.a.furr@gmail.com>
2024-06-14 00:22:49 -0400

    updated todo

--- server/internal/routeHandling/makeGuess.go
+++ server/internal/routeHandling/makeGuess.go
@@ -2,18 +2,29 @@ package routeHandling
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"spelldle.com/server/shared/types"
 )
 
 func (r *RouteHandler) MakeGuessCategories(ctx *gin.Context) {
 	var payload types.SpellCategories
+	var response types.ResponseMakeGuess
 	if err := ctx.BindJSON(&payload); err != nil {
 		fmt.Printf("Error binding payload: %v\n", err)
 		ctx.String(http.StatusInternalServerError, "Invalid payload")
 		return
 	}
-	fmt.Printf("PAYLOAD: %v\n", payload)
-	ctx.String(http.StatusOK, "Success")
+	fmt.Printf("/api/makeGuess PAYLOAD: %v\n", payload)
+
+	// Get spell to compare (hardwired for now)
+	// TODO: pull in currentSpell from users.currentGameData
+	spell, err := r.dbHandler.GetSpellBySpellId(0)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, response)
 }
