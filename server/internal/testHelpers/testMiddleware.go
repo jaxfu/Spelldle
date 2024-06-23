3460be2f56c1d68a0bbb18359314a1bb6b7ebc1f
Jackson Furr <jackson.a.furr@gmail.com>
2024-06-23 11:14:21 -0400

    middleware test implemented

--- server/internal/testHelpers/testMiddleware.go
+++ server/internal/testHelpers/testMiddleware.go
@@ -15,8 +15,6 @@ type emptyStruct struct {
 	A string
 }
 
-var nilUrl = ""
-
 func TestMiddleware(tokens types.AllTokens, middleware gin.HandlerFunc) (*gin.Context, error) {
 	gin.SetMode(gin.TestMode)
 	w := httptest.NewRecorder()
@@ -27,15 +25,16 @@ func TestMiddleware(tokens types.AllTokens, middleware gin.HandlerFunc) (*gin.Co
 		return ctx, fmt.Errorf("error marshalling payload: %+vn", err)
 	}
 
-	r, err := http.NewRequest(http.MethodPost, nilUrl, bytes.NewReader(marshalled))
+	r, err := http.NewRequest(http.MethodPost, consts.RouteUrlBase, bytes.NewReader(marshalled))
 	if err != nil {
 		return ctx, fmt.Errorf("error creating request: %+v", err)
 	}
 	r.Header.Set(consts.HeaderTypeAuthorization, fmt.Sprintf("%s%s", consts.BearerTokenPrefix, tokens.AccessToken.AccessToken))
 	fmt.Printf("HEADER: %+v\n", r.Header.Get(consts.HeaderTypeAuthorization))
 	router.Use(middleware)
-	router.POST("/", func(c *gin.Context) {
+	router.POST(consts.RouteUrlBase, func(c *gin.Context) {
 		// This handler does nothing but ensures the middleware gets executed
+		ctx = c
 	})
 	router.ServeHTTP(w, r)
 
