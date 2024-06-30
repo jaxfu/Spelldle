00798dea8b0dd527ba5b9d1f0d9148e84860e555
Jackson Furr <jackson.a.furr@gmail.com>
2024-06-30 11:57:32 -0400

    init ResultBox

---
+++ client/src/components/Game/children/ResultBox/ResultBox.tsx
@@ -0,0 +1,7 @@
+import styles from "./ResultBox.module.scss";
+
+const ResultBox: React.FC = () => {
+	return <div className={styles.root}>ResultBox</div>;
+};
+
+export default ResultBox;
---
+++ client/src/components/Game/children/ResultBox/children/ResultCell/ResultCell.tsx
@@ -0,0 +1,7 @@
+import styles from "./ResultCell.module.scss";
+
+const ResultCell: React.FC = () => {
+	return <div className={styles.root}>ResultCell</div>;
+};
+
+export default ResultCell;
