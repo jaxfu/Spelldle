b528ddf0d70320521dba36fcf0e3e701c3aa7ea5
Jackson Furr <jackson.a.furr@gmail.com>
2024-06-30 23:12:29 -0400

    db sql refactor

--- config/sql/create_tables.sql
+++ config/sql/create_tables.sql
@@ -15,12 +15,19 @@ CREATE TABLE spells.ids
 
 CREATE TABLE game_sessions.ids
 (
-  game_session_id SERIAL PRIMARY KEY
+  game_session_id TEXT PRIMARY KEY
+);
+
+CREATE TABLE guesses.ids
+(
+  game_session_id TEXT REFERENCES game_sessions.ids(game_session_id),
+  round SMALLINT,
+  PRIMARY KEY(game_session_id, round)
 );
 
 CREATE TABLE spells.categories
 (
-  spell_id INTEGER PRIMARY KEY UNIQUE REFERENCES spells.ids(spell_id),
+  spell_id INTEGER PRIMARY KEY REFERENCES spells.ids(spell_id),
   name TEXT UNIQUE,
   school INTEGER,
   casting_time INTEGER,
@@ -34,7 +41,7 @@ CREATE TABLE spells.categories
 
 CREATE TABLE spells.level_objects
 (
-  spell_id INTEGER PRIMARY KEY UNIQUE references spells.ids(spell_id),
+  spell_id INTEGER PRIMARY KEY references spells.ids(spell_id),
   level INTEGER,
   is_ritual BOOLEAN
 );
@@ -42,7 +49,7 @@ CREATE TABLE spells.level_objects
 CREATE TABLE game_sessions.data
 (
   user_id INTEGER REFERENCES users.ids(user_id),
-  game_session_id INTEGER REFERENCES game_sessions.ids(game_session_id),
+  game_session_id TEXT REFERENCES game_sessions.ids(game_session_id),
   spell_id INTEGER REFERENCES spells.ids(spell_id),
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP,
@@ -55,13 +62,18 @@ CREATE TABLE users.data
     username   VARCHAR(32) UNIQUE,
     password   VARCHAR(32),
     first_name VARCHAR(32),
-    last_name  VARCHAR(32),
-    game_session_id INTEGER REFERENCES game_sessions.ids(game_session_id)
+    last_name  VARCHAR(32)
+);
+
+CREATE TABLE users.game_sessions
+(
+  user_id INTEGER PRIMARY KEY REFERENCES users.ids(user_id),
+  game_session_id TEXT REFERENCES game_sessions.ids(game_session_id)
 );
 
 CREATE TABLE guesses.categories
 (
-  game_session_id INTEGER REFERENCES game_sessions.ids(game_session_id),
+  game_session_id TEXT,
   round SMALLINT,
   school INTEGER,
   casting_time INTEGER,
@@ -71,21 +83,23 @@ CREATE TABLE guesses.categories
   components INTEGER[],
   class INTEGER[],
   effects INTEGER[],
-  PRIMARY KEY(game_session_id, round)
+  PRIMARY KEY(game_session_id, round),
+  FOREIGN KEY(game_session_id, round) REFERENCES guesses.ids(game_session_id, round)
 );
 
 CREATE TABLE guesses.level_objects
 (
-  game_session_id INTEGER REFERENCES game_sessions.ids(game_session_id),
+  game_session_id TEXT,
   round SMALLINT,
   level INTEGER,
   is_ritual BOOLEAN,
-  PRIMARY KEY(game_session_id, round)
+  PRIMARY KEY(game_session_id, round),
+  FOREIGN KEY(game_session_id, round) REFERENCES guesses.ids(game_session_id, round)
 );
 
 CREATE TABLE guesses.results
 (
-  game_session_id INTEGER REFERENCES game_sessions.ids(game_session_id),
+  game_session_id TEXT,
   round SMALLINT,
   school SMALLINT,
   casting_time SMALLINT,
@@ -96,5 +110,6 @@ CREATE TABLE guesses.results
   components SMALLINT,
   class SMALLINT,
   effects SMALLINT,
-  PRIMARY KEY(game_session_id, round)
+  PRIMARY KEY(game_session_id, round),
+  FOREIGN KEY(game_session_id, round) REFERENCES guesses.ids(game_session_id, round)
 );
--- config/sql/seed_db.sql
+++ config/sql/seed_db.sql
@@ -1,6 +1,11 @@
-INSERT INTO users.ids DEFAULT VALUES;
-INSERT INTO spells.ids(spell_id) VALUES (1);
-INSERT INTO game_sessions.ids DEFAULT VALUES;
+INSERT INTO users.ids
+DEFAULT VALUES;
+INSERT INTO spells.ids(spell_id)
+VALUES (1);
+INSERT INTO game_sessions.ids(game_session_id)
+VALUES ('TEST_ID');
+INSERT INTO guesses.ids(game_session_id, round)
+VALUES ('TEST_ID', 1);
 
 INSERT INTO spells.categories(spell_id, name, school, casting_time, range, target, duration, components, class, effects)
 VALUES (1, 'Fireball', 0, 0, 0, 0, 0, '{0, 1}', '{1, 2}', '{3, 4}');
@@ -8,17 +13,20 @@ VALUES (1, 'Fireball', 0, 0, 0, 0, 0, '{0, 1}', '{1, 2}', '{3, 4}');
 INSERT INTO spells.level_objects(spell_id, level, is_ritual)
 VALUES (1, 1, false);
 
-INSERT INTO users.data(user_id, username, password, first_name, last_name, game_session_id)
-VALUES (1, 'poemmys', 'pass', 'Jackson', 'Furr', 1);
+INSERT INTO users.data(user_id, username, password, first_name, last_name)
+VALUES (1, 'poemmys', 'pass', 'Jackson', 'Furr');
+
+INSERT INTO users.game_sessions(user_id, game_session_id)
+VALUES (1, 'TEST_ID');
 
 INSERT INTO game_sessions.data(game_session_id, user_id, spell_id, rounds)
-VALUES (1, 1, 1, 2);
+VALUES ('TEST_ID', 1, 1, 2);
 
 INSERT INTO guesses.categories(game_session_id, round, school, casting_time, range, target, duration, components, class, effects)
-VALUES (1, 1, 1, 0, 0, 0, 0, '{0, 1}', '{1, 2}', '{3, 4}');
+VALUES ('TEST_ID', 1, 1, 0, 0, 0, 0, '{0, 1}', '{1, 2}', '{3, 4}');
 
 INSERT INTO guesses.level_objects(game_session_id, round, level, is_ritual)
-VALUES (1, 1, 1, false);
+VALUES ('TEST_ID', 1, 1, false);
 
 INSERT INTO guesses.results(game_session_id, round, school, casting_time, range, target, duration, level, components, class, effects)
-VALUES (1, 1, 0, 2, 2, 2, 2, 2, 2, 2, 2);
+VALUES ('TEST_ID', 1, 0, 2, 2, 2, 2, 2, 2, 2, 2);
