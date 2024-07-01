7eeef42c21de5273484663075db4f62f8ac21761
Jackson Furr <jackson.a.furr@gmail.com>
2024-06-30 23:16:20 -0400

    db refactored

--- server/shared/dbHandler/dbHandler_test.go
+++ server/shared/dbHandler/dbHandler_test.go
@@ -22,9 +22,7 @@ func TestDBHandler(t *testing.T) {
 	dbHandler := InitDBHandler(os.Getenv("DB_URL_TEST"))
 	defer dbHandler.Conn.Close()
 
-	testUserID := testHelpers.TestUserDataAll.UserID
-	testUserDataAccount := testHelpers.TestUserDataAll.UserDataAccount
-	testUserDataPersonal := testHelpers.TestUserDataAll.UserDataPersonal
+	testUserDataAll := testHelpers.TestUserDataAll
 
 	t.Run("Ping connection", func(t *testing.T) {
 		if err := dbHandler.Conn.Ping(context.Background()); err != nil {
@@ -48,44 +46,56 @@ func TestDBHandler(t *testing.T) {
 			t.Errorf("Error in InsertUser: %+v\n", err)
 		}
 		if userId != 1 {
-			t.Errorf("Inserted user id should be %d, but got %d\n", testUserID, userId)
+			t.Errorf("Inserted user id should be %d, but got %d\n", testUserDataAll.UserID, userId)
 		}
 	})
 
-	t.Run("InsertUserDataAccount", func(t *testing.T) {
-		if err := dbHandler.InsertUserDataAccount(testUserID, testUserDataAccount); err != nil {
-			t.Errorf("Error in InsertUserDataAccount: %+v\n", err)
+	t.Run("InsertUserDataAll", func(t *testing.T) {
+		if err := dbHandler.InsertUserDataAll(testUserDataAll); err != nil {
+			t.Errorf("Error in InsertUserDataAll: %+v\n", err)
 		}
 	})
-	t.Run("GetUserDataAccountByUserID", func(t *testing.T) {
-		userDataAccount, err := dbHandler.GetUserDataAccountByUserID(testUserID)
+	t.Run("GetUserDataAllByUserID", func(t *testing.T) {
+		userDataAll, err := dbHandler.GetUserDataAllByUserID(testUserDataAll.UserID)
 		if err != nil {
-			t.Errorf("Error in GetUserDataAccountByUserID: %+v\n", err)
+			t.Errorf("Error in GetUserDataAllByUserID: %+v\n", err)
 		}
-		if userDataAccount != testUserDataAccount {
-			t.Errorf("Mismatch in GetUserDataAccountByUserID: got %+v, want %+v", userDataAccount, testUserDataAccount)
+		if userDataAll != testUserDataAll {
+			t.Errorf("Mismatch in GetUserDataAllByUserID: got %+v, want %+v", userDataAll, testUserDataAll)
 		}
 	})
+	// t.Run("GetUserDataAccountByUserID", func(t *testing.T) {
+	// 	userDataAll, err := dbHandler.GetUserDataAllByUserID(testUserDataAll.UserID)
+	// 	if err != nil {
+	// 		t.Errorf("Error in GetUserDataAllByUserID: %+v\n", err)
+	// 	}
+	// 	if userDataAll != testUserDataAll {
+	// 		t.Errorf("Mismatch in GetUserDataAccountByUserID: got %+v, want %+v", userDataAll, testUserDataAll)
+	// 	}
+	// })
 
-	t.Run("InsertUserDataPersonal", func(t *testing.T) {
-		if err := dbHandler.InsertUserDataPersonal(testUserID, testUserDataPersonal); err != nil {
-			t.Errorf("Error in InsertUserDataPersonal: %+v\n", err)
-		}
-	})
-	t.Run("GetUserDataPersonalByUserID", func(t *testing.T) {
-		userDataPersonal, err := dbHandler.GetUserDataPersonalByUserID(testUserID)
-		if err != nil {
-			t.Errorf("Error in GetUserDataPersonalByUserID: %+v\n", err)
-		}
-		if userDataPersonal != testUserDataPersonal {
-			t.Errorf("Mismatch in GetUserDataPersonalByUserID: got %+v, want %+v", userDataPersonal, testUserDataPersonal)
-		}
-	})
+	// t.Run("InsertUserDataPersonal", func(t *testing.T) {
+	// 	if err := dbHandler.InsertUserDataPersonal(testUserID, testUserDataPersonal); err != nil {
+	// 		t.Errorf("Error in InsertUserDataPersonal: %+v\n", err)
+	// 	}
+	// })
+	// t.Run("GetUserDataPersonalByUserID", func(t *testing.T) {
+	// 	userDataPersonal, err := dbHandler.GetUserDataPersonalByUserID(testUserID)
+	// 	if err != nil {
+	// 		t.Errorf("Error in GetUserDataPersonalByUserID: %+v\n", err)
+	// 	}
+	// 	if userDataPersonal != testUserDataPersonal {
+	// 		t.Errorf("Mismatch in GetUserDataPersonalByUserID: got %+v, want %+v", userDataPersonal, testUserDataPersonal)
+	// 	}
+	// })
 
 	t.Run("GetUserIDByUsernameValid", func(t *testing.T) {
-		_, err := dbHandler.GetUserIDByUsername(testUserDataAccount.Username)
+		userID, err := dbHandler.GetUserIDByUsername(testUserDataAll.Username)
 		if err != nil {
 			t.Errorf("Error in GetUserIDByUsername: %+v\n", err)
+		}
+		if userID != testUserDataAll.UserID {
+			t.Errorf("Invalid UserID: got %d, want %d", userID, testUserDataAll.UserID)
 		}
 	})
 	t.Run("GetUserIDByUsernameInvalid", func(t *testing.T) {
