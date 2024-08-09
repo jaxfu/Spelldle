e2ca1f578773854a263306f2aca2e79b01f8f9dc
Jackson Furr <jackson.a.furr@gmail.com>
2024-08-08 20:43:29 -0400

    test_guesses working

--- client/src/components/DEBUG/GuessInfoButton/GuessInfoButton.tsx
+++ client/src/components/DEBUG/GuessInfoButton/GuessInfoButton.tsx
@@ -1,7 +1,7 @@
 import styles from "./GuessInfoButton.module.scss";
 import { useContext } from "react";
 import GuessDataContext from "../../../Contexts/GuessDataContext";
-import { TEST_GUESS_CORRECT } from "./TEST_GUESSES";
+import * as testGuesses from "./TEST_GUESSES";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { apiRequestMakeGuess } from "../../../methods/requests";
 import { QUERY_KEYS } from "../../../utils/consts";
@@ -9,44 +9,44 @@ import { getUserSessionDataFromStorage } from "../../../utils/methods";
 import { T_GUESS_CATEGORIES_MAP } from "../../../methods/guesses";
 
 const GuessInfoButton: React.FC = () => {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-  const mutation = useMutation({
-    mutationFn: apiRequestMakeGuess,
-    onSuccess: (data) => {
-      console.log("SUCCESFUL MAKE_GUESS: " + data.data.toString());
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.pastGuesses] });
-    },
-  });
+	const mutation = useMutation({
+		mutationFn: apiRequestMakeGuess,
+		onSuccess: (data) => {
+			console.log("SUCCESFUL MAKE_GUESS: " + data.data.toString());
+			queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.pastGuesses] });
+		},
+	});
 
-  const guessData = useContext(GuessDataContext);
+	const guessData = useContext(GuessDataContext);
 
-  const testGuess: T_GUESS_CATEGORIES_MAP = new Map(
-    Object.entries(TEST_GUESS_CORRECT),
-  );
+	const testGuess: T_GUESS_CATEGORIES_MAP = new Map(
+		Object.entries(testGuesses.TEST_GUESS_SOME_CORRECT),
+	);
 
-  return (
-    <div className={styles.root}>
-      <button
-        onClick={() => {
-          console.log("CURRENT GUESSINFO: ");
-          guessData && console.log(guessData?.current);
-        }}
-      >
-        Log
-      </button>
-      <button
-        onClick={() => {
-          mutation.mutate({
-            accessToken: getUserSessionDataFromStorage().access_token,
-            guessData: testGuess,
-          });
-        }}
-      >
-        Guess
-      </button>
-    </div>
-  );
+	return (
+		<div className={styles.root}>
+			<button
+				onClick={() => {
+					console.log("CURRENT GUESSINFO: ");
+					guessData && console.log(guessData?.current);
+				}}
+			>
+				Log
+			</button>
+			<button
+				onClick={() => {
+					mutation.mutate({
+						accessToken: getUserSessionDataFromStorage().access_token,
+						guessData: testGuess,
+					});
+				}}
+			>
+				Guess
+			</button>
+		</div>
+	);
 };
 
 export default GuessInfoButton;
