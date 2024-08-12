249b78e436482cd7f903d4ce21371144661b3742
Jackson Furr <jackson.a.furr@gmail.com>
2024-08-12 16:58:33 -0400

    wip

--- client/src/components/Game/children/ResultBox/ResultBox.tsx
+++ client/src/components/Game/children/ResultBox/ResultBox.tsx
@@ -4,24 +4,30 @@ import Result from "./children/Result/Result";
 import styles from "./ResultBox.module.scss";
 
 interface IProps {
-	pastGuesses: T_GUESS_AND_RESULT[];
-	categoriesInfoArr: T_CATEGORY_INFO[];
+  pastGuesses: T_GUESS_AND_RESULT[];
+  categoriesInfoArr: T_CATEGORY_INFO[];
 }
 
 const ResultBox: React.FC<IProps> = (props) => {
-	return (
-		<div className={styles.root}>
-			{props.pastGuesses.map((guess) => {
-				return (
-					<Result
-						guess={guess}
-						key={guess.round}
-						categoriesInfoArr={props.categoriesInfoArr}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className={styles.root}>
+      <span className={styles.cell}><b>Round</b></span>
+      {props.categoriesInfoArr.map((category) => {
+        return (
+          <span className={styles.cell}><b>{category.name}</b></span>
+        )
+      })}
+      {/* {props.pastGuesses.map((guess) => { */}
+      {/* 	return ( */}
+      {/* 		<Result */}
+      {/* 			guess={guess} */}
+      {/* 			key={guess.round} */}
+      {/* 			categoriesInfoArr={props.categoriesInfoArr} */}
+      {/* 		/> */}
+      {/* 	); */}
+      {/* })} */}
+    </div>
+  );
 };
 
 export default ResultBox;
