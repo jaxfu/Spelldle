bf3fd7c2a214111752ff92b920b9b87eeac46f24
Jackson Furr <jackson.a.furr@gmail.com>
2024-08-19 11:10:23 -0400

    formatting

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -6,135 +6,135 @@ import { apiRequestGetPastGuesses } from "../../types/requests";
 import { useEffect, useMemo, useRef, useState } from "react";
 import GuessInfoButton from "../DEBUG/GuessInfoButton/GuessInfoButton";
 import {
-  E_CATEGORY_COMPONENT_TYPE,
-  type T_CATEGORY_INFO,
-  type T_CATEGORY_INFO_SEED_JSON,
-  generateCategoryInfoFromSeedJSON,
-  generateGuessesStateFromJSON,
+	E_CATEGORY_COMPONENT_TYPE,
+	type T_CATEGORY_INFO,
+	type T_CATEGORY_INFO_SEED_JSON,
+	generateCategoryInfoFromSeedJSON,
+	generateGuessesStateFromJSON,
 } from "../../types/categories";
 import CATEGORY_INFO_JSON from "../../data/CATEGORY_INFO.json";
 import CtxGuessData from "../../contexts/CtxGuessData";
 import CtxGuessCellsState from "../../contexts/CtxGuessCellsState";
 import ResultBox from "./children/ResultBox/ResultBox";
 import {
-  E_RESULT_OPTIONS,
-  T_GUESSES_AS_IDS,
-  T_PAST_GUESS,
-  translateIdsToValues,
+	E_RESULT_OPTIONS,
+	T_GUESSES_AS_IDS,
+	T_PAST_GUESS,
+	translateIdsToValues,
 } from "../../types/guesses";
 import { I_GUESS_CELL_STATE } from "./children/GuessBox/children/GuessCell/GuessCell";
 
 const Game: React.FC = () => {
-  const categoriesInfo: T_CATEGORY_INFO[] = useMemo(() => {
-    return generateCategoryInfoFromSeedJSON(
-      CATEGORY_INFO_JSON as T_CATEGORY_INFO_SEED_JSON,
-    );
-  }, []);
-  const currentGuessInfo = useRef<T_GUESSES_AS_IDS>(
-    generateGuessesStateFromJSON(
-      CATEGORY_INFO_JSON as T_CATEGORY_INFO_SEED_JSON,
-    ),
-  );
-  const [guessCellsState, setGuessCellsState] = useState<
-    Map<string, I_GUESS_CELL_STATE>
-  >(initGuessCellsStateMap(categoriesInfo));
+	const categoriesInfo: T_CATEGORY_INFO[] = useMemo(() => {
+		return generateCategoryInfoFromSeedJSON(
+			CATEGORY_INFO_JSON as T_CATEGORY_INFO_SEED_JSON,
+		);
+	}, []);
+	const currentGuessInfo = useRef<T_GUESSES_AS_IDS>(
+		generateGuessesStateFromJSON(
+			CATEGORY_INFO_JSON as T_CATEGORY_INFO_SEED_JSON,
+		),
+	);
+	const [guessCellsState, setGuessCellsState] = useState<
+		Map<string, I_GUESS_CELL_STATE>
+	>(initGuessCellsStateMap(categoriesInfo));
 
-  const { data, isSuccess, isFetching } = useQuery({
-    queryKey: [QUERY_KEYS.pastGuesses],
-    queryFn: () =>
-      apiRequestGetPastGuesses(getUserSessionDataFromStorage().access_token),
-  });
+	const { data, isSuccess, isFetching } = useQuery({
+		queryKey: [QUERY_KEYS.pastGuesses],
+		queryFn: () =>
+			apiRequestGetPastGuesses(getUserSessionDataFromStorage().access_token),
+	});
 
-  // if (isSuccess && data.data.guesses.length > 0) {
-  //   console.log(JSON.stringify(data.data));
-  // }
+	// if (isSuccess && data.data.guesses.length > 0) {
+	//   console.log(JSON.stringify(data.data));
+	// }
 
-  useEffect(() => {
-    console.log("updating guess cells state");
-    if (isSuccess && data.data.guesses.length > 0) {
-      setGuessCellsState((state) =>
-        updateGuessCellsStateMap(
-          categoriesInfo,
-          data.data.guesses[data.data.guesses.length - 1],
-        ),
-      );
-    }
-  }, [isSuccess, isFetching]);
+	useEffect(() => {
+		console.log("updating guess cells state");
+		if (isSuccess && data.data.guesses.length > 0) {
+			setGuessCellsState((state) =>
+				updateGuessCellsStateMap(
+					categoriesInfo,
+					data.data.guesses[data.data.guesses.length - 1],
+				),
+			);
+		}
+	}, [isSuccess, isFetching]);
 
-  function initGuessCellsStateMap(
-    categoriesInfo: T_CATEGORY_INFO[],
-  ): Map<string, I_GUESS_CELL_STATE> {
-    const map = new Map();
+	function initGuessCellsStateMap(
+		categoriesInfo: T_CATEGORY_INFO[],
+	): Map<string, I_GUESS_CELL_STATE> {
+		const map = new Map();
 
-    for (const { id, component_type } of categoriesInfo) {
-      let guessCellState: I_GUESS_CELL_STATE;
+		for (const { id, component_type } of categoriesInfo) {
+			let guessCellState: I_GUESS_CELL_STATE;
 
-      switch (component_type) {
-        case E_CATEGORY_COMPONENT_TYPE.SINGLE_TEXT:
-          guessCellState = {
-            input: "",
-            result: E_RESULT_OPTIONS.UNINITIALIZED,
-          };
-          break;
-        case E_CATEGORY_COMPONENT_TYPE.MULTI_TEXT:
-        case E_CATEGORY_COMPONENT_TYPE.COMPONENTS:
-          guessCellState = {
-            input: [],
-            result: E_RESULT_OPTIONS.UNINITIALIZED,
-          };
-          break;
-        case E_CATEGORY_COMPONENT_TYPE.LEVEL:
-          guessCellState = {
-            input: { level: "", is_ritual: false },
-            result: E_RESULT_OPTIONS.UNINITIALIZED,
-          };
-          break;
-      }
+			switch (component_type) {
+				case E_CATEGORY_COMPONENT_TYPE.SINGLE_TEXT:
+					guessCellState = {
+						input: "",
+						result: E_RESULT_OPTIONS.UNINITIALIZED,
+					};
+					break;
+				case E_CATEGORY_COMPONENT_TYPE.MULTI_TEXT:
+				case E_CATEGORY_COMPONENT_TYPE.COMPONENTS:
+					guessCellState = {
+						input: [],
+						result: E_RESULT_OPTIONS.UNINITIALIZED,
+					};
+					break;
+				case E_CATEGORY_COMPONENT_TYPE.LEVEL:
+					guessCellState = {
+						input: { level: "", is_ritual: false },
+						result: E_RESULT_OPTIONS.UNINITIALIZED,
+					};
+					break;
+			}
 
-      map.set(id, guessCellState);
-    }
+			map.set(id, guessCellState);
+		}
 
-    return map;
-  }
+		return map;
+	}
 
-  function updateGuessCellsStateMap(
-    categoriesInfo: T_CATEGORY_INFO[],
-    pastGuess: T_PAST_GUESS,
-  ): Map<string, I_GUESS_CELL_STATE> {
-    const map = new Map();
+	function updateGuessCellsStateMap(
+		categoriesInfo: T_CATEGORY_INFO[],
+		pastGuess: T_PAST_GUESS,
+	): Map<string, I_GUESS_CELL_STATE> {
+		const map = new Map();
 
-    for (const category of categoriesInfo) {
-      const guessMap: T_PAST_GUESS = new Map(Object.entries(pastGuess));
-      const guess = guessMap.get(category.id);
-      if (guess !== undefined) {
-        map.set(category.id, {
-          input: translateIdsToValues(guess.value, category),
-          result: guess?.result,
-        });
-      }
-    }
+		for (const category of categoriesInfo) {
+			const guessMap: T_PAST_GUESS = new Map(Object.entries(pastGuess));
+			const guess = guessMap.get(category.id);
+			if (guess !== undefined) {
+				map.set(category.id, {
+					input: translateIdsToValues(guess.value, category),
+					result: guess?.result,
+				});
+			}
+		}
 
-    return map;
-  }
+		return map;
+	}
 
-  return (
-    <>
-      <CtxGuessData.Provider value={currentGuessInfo}>
-        <CtxGuessCellsState.Provider
-          value={{ state: guessCellsState, setState: setGuessCellsState }}
-        >
-          <GuessInfoButton />
-          {data != undefined && data.data.guesses.length > 0 && (
-            <ResultBox
-              pastGuesses={data.data.guesses}
-              categoriesInfoArr={categoriesInfo}
-            />
-          )}
-          <GuessBox categoriesInfoArr={categoriesInfo} />
-        </CtxGuessCellsState.Provider>
-      </CtxGuessData.Provider>
-    </>
-  );
+	return (
+		<>
+			<CtxGuessData.Provider value={currentGuessInfo}>
+				<CtxGuessCellsState.Provider
+					value={{ state: guessCellsState, setState: setGuessCellsState }}
+				>
+					<GuessInfoButton />
+					{data != undefined && data.data.guesses.length > 0 && (
+						<ResultBox
+							pastGuesses={data.data.guesses}
+							categoriesInfoArr={categoriesInfo}
+						/>
+					)}
+					<GuessBox categoriesInfoArr={categoriesInfo} />
+				</CtxGuessCellsState.Provider>
+			</CtxGuessData.Provider>
+		</>
+	);
 };
 
 export default Game;
--- client/src/components/Game/children/GuessBox/children/GuessCell/children/SingleText/SingleText.tsx
+++ client/src/components/Game/children/GuessBox/children/GuessCell/children/SingleText/SingleText.tsx
@@ -2,134 +2,134 @@ import TextInput from "./children/TextInput/TextInput";
 import RecommendationBox from "../RecommendationBox/RecommendationBox";
 import { useState, useMemo, useContext, useEffect } from "react";
 import {
-  E_CATEGORY_COMPONENT_TYPE,
-  type T_CATEGORY_INFO,
+	E_CATEGORY_COMPONENT_TYPE,
+	type T_CATEGORY_INFO,
 } from "../../../../../../../../types/categories";
 import CtxGuessData from "../../../../../../../../contexts/CtxGuessData";
 import {
-  type T_GUESS_STATES_IDS_LEVEL,
-  type T_GUESSES_AS_IDS,
-  translateIdsToValues,
+	type T_GUESS_STATES_IDS_LEVEL,
+	type T_GUESSES_AS_IDS,
+	translateIdsToValues,
 } from "../../../../../../../../types/guesses";
 import CtxGuessCellsState, {
-  I_CTX_GUESS_CELLS_STATE,
+	I_CTX_GUESS_CELLS_STATE,
 } from "../../../../../../../../contexts/CtxGuessCellsState";
 
 function updateGuessCategoriesMapSingleText(
-  input: string,
-  hasValidInput: boolean,
-  categoryInfo: T_CATEGORY_INFO,
-  guessData: React.MutableRefObject<T_GUESSES_AS_IDS> | null,
+	input: string,
+	hasValidInput: boolean,
+	categoryInfo: T_CATEGORY_INFO,
+	guessData: React.MutableRefObject<T_GUESSES_AS_IDS> | null,
 ): void {
-  if (guessData !== null) {
-    if (hasValidInput) {
-      const valueId = categoryInfo.value_id_map.get(input.toLowerCase());
+	if (guessData !== null) {
+		if (hasValidInput) {
+			const valueId = categoryInfo.value_id_map.get(input.toLowerCase());
 
-      if (valueId !== undefined) {
-        guessData.current.set(categoryInfo.id, valueId);
-      }
-    } else {
-      guessData.current.set(categoryInfo.id, -1);
-    }
-  }
+			if (valueId !== undefined) {
+				guessData.current.set(categoryInfo.id, valueId);
+			}
+		} else {
+			guessData.current.set(categoryInfo.id, -1);
+		}
+	}
 }
 
 function updateGuessCategoriesMapLevelText(
-  input: string,
-  hasValidInput: boolean,
-  categoryInfo: T_CATEGORY_INFO,
-  guessData: React.MutableRefObject<T_GUESSES_AS_IDS> | null,
+	input: string,
+	hasValidInput: boolean,
+	categoryInfo: T_CATEGORY_INFO,
+	guessData: React.MutableRefObject<T_GUESSES_AS_IDS> | null,
 ): void {
-  if (guessData !== null) {
-    const currentData = guessData.current.get(
-      categoryInfo.id,
-    ) as T_GUESS_STATES_IDS_LEVEL;
+	if (guessData !== null) {
+		const currentData = guessData.current.get(
+			categoryInfo.id,
+		) as T_GUESS_STATES_IDS_LEVEL;
 
-    if (hasValidInput) {
-      const valueId = categoryInfo.value_id_map.get(input.toLowerCase());
+		if (hasValidInput) {
+			const valueId = categoryInfo.value_id_map.get(input.toLowerCase());
 
-      if (valueId !== undefined && currentData !== undefined) {
-        guessData.current.set(categoryInfo.id, {
-          level: valueId,
-          is_ritual: currentData.is_ritual,
-        });
-      }
-    } else {
-      guessData.current.set(categoryInfo.id, {
-        level: -1,
-        is_ritual: currentData.is_ritual,
-      });
-    }
-  }
+			if (valueId !== undefined && currentData !== undefined) {
+				guessData.current.set(categoryInfo.id, {
+					level: valueId,
+					is_ritual: currentData.is_ritual,
+				});
+			}
+		} else {
+			guessData.current.set(categoryInfo.id, {
+				level: -1,
+				is_ritual: currentData.is_ritual,
+			});
+		}
+	}
 }
 
 interface IProps {
-  categoryInfo: T_CATEGORY_INFO;
+	categoryInfo: T_CATEGORY_INFO;
 }
 
 const SingleText: React.FC<IProps> = (props) => {
-  const [input, setInput] = useState<string>("");
-  const [show, setShow] = useState<boolean>(false);
+	const [input, setInput] = useState<string>("");
+	const [show, setShow] = useState<boolean>(false);
 
-  const guessData = useContext(CtxGuessData);
-  const cellState = useContext(CtxGuessCellsState)
+	const guessData = useContext(CtxGuessData);
+	const cellState = useContext(CtxGuessCellsState);
 
-  const hasValidInput: boolean = useMemo(() => {
-    return props.categoryInfo.value_id_map.has(input.toLowerCase());
-  }, [input]);
+	const hasValidInput: boolean = useMemo(() => {
+		return props.categoryInfo.value_id_map.has(input.toLowerCase());
+	}, [input]);
 
-  // update guessData map on valid input detection
-  useEffect(() => {
-    if (
-      props.categoryInfo.component_type ===
-      E_CATEGORY_COMPONENT_TYPE.SINGLE_TEXT
-    ) {
-      updateGuessCategoriesMapSingleText(
-        input,
-        hasValidInput,
-        props.categoryInfo,
-        guessData,
-      );
-    } else if (
-      props.categoryInfo.component_type === E_CATEGORY_COMPONENT_TYPE.LEVEL
-    ) {
-      updateGuessCategoriesMapLevelText(
-        input,
-        hasValidInput,
-        props.categoryInfo,
-        guessData,
-      );
-    }
-  }, [hasValidInput]);
+	// update guessData map on valid input detection
+	useEffect(() => {
+		if (
+			props.categoryInfo.component_type ===
+			E_CATEGORY_COMPONENT_TYPE.SINGLE_TEXT
+		) {
+			updateGuessCategoriesMapSingleText(
+				input,
+				hasValidInput,
+				props.categoryInfo,
+				guessData,
+			);
+		} else if (
+			props.categoryInfo.component_type === E_CATEGORY_COMPONENT_TYPE.LEVEL
+		) {
+			updateGuessCategoriesMapLevelText(
+				input,
+				hasValidInput,
+				props.categoryInfo,
+				guessData,
+			);
+		}
+	}, [hasValidInput]);
 
-  useEffect(() => {
-    if (cellState !== null) {
-      const info = cellState.state.get(props.categoryInfo.id)
-      console.log(info)
-      if (info !== undefined) {
-        setInput((input) => info.input.toString())
-      }
-    }
-  }, [cellState])
+	useEffect(() => {
+		if (cellState !== null) {
+			const info = cellState.state.get(props.categoryInfo.id);
+			console.log(info);
+			if (info !== undefined) {
+				setInput((input) => info.input.toString());
+			}
+		}
+	}, [cellState]);
 
-  return (
-    <>
-      <TextInput
-        input={input}
-        setInput={setInput}
-        show={show}
-        setShow={setShow}
-        validInput={hasValidInput}
-      />
-      {show && (
-        <RecommendationBox
-          values={props.categoryInfo.values}
-          input={input}
-          setInput={setInput}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			<TextInput
+				input={input}
+				setInput={setInput}
+				show={show}
+				setShow={setShow}
+				validInput={hasValidInput}
+			/>
+			{show && (
+				<RecommendationBox
+					values={props.categoryInfo.values}
+					input={input}
+					setInput={setInput}
+				/>
+			)}
+		</>
+	);
 };
 
 export default SingleText;
