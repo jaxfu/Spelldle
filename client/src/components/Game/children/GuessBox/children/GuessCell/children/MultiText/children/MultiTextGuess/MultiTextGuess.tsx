6bd49e6bfbbfefdae4a2c697a8797fc4c1f5cc80
Jackson Furr <jackson.a.furr@gmail.com>
2024-08-31 11:22:40 -0400

    remove from guesses working

--- client/src/components/Game/children/GuessBox/children/GuessCell/children/MultiText/children/MultiTextGuess/MultiTextGuess.tsx
+++ client/src/components/Game/children/GuessBox/children/GuessCell/children/MultiText/children/MultiTextGuess/MultiTextGuess.tsx
@@ -2,17 +2,27 @@ import styles from "./MultiTextGuess.module.scss";
 import { useState, useRef, useEffect } from "react";
 
 interface IProps {
-	content: string;
+	guess: string;
+	IRemoveFromGuesses: {
+		removeFromGuesses(
+			guess: string,
+			setGuesses: React.Dispatch<React.SetStateAction<string[]>>,
+			remainingReccomendations: React.MutableRefObject<string[]>,
+		): void;
+		setGuesses: React.Dispatch<React.SetStateAction<string[]>>;
+		remainingReccomendations: React.MutableRefObject<string[]>;
+	};
 }
 
 const MultiTextGuess: React.FC<IProps> = (props) => {
-	const [content, setContent] = useState<string>(props.content);
+	const [content, setContent] = useState<string>(props.guess);
 	const [contentWidth, setContentWidth] = useState<number>(0);
 	const ref = useRef<HTMLDivElement>(null);
 
+	// set contentWidth on load
 	useEffect(() => {
 		if (ref.current) {
-			if (content === props.content && contentWidth === 0) {
+			if (content === props.guess && contentWidth === 0) {
 				setContentWidth(ref.current.offsetWidth);
 			} else {
 				ref.current.style.width = `${contentWidth}px`;
@@ -20,19 +30,27 @@ const MultiTextGuess: React.FC<IProps> = (props) => {
 		}
 	}, [ref.current, content]);
 
+	// set ref width to contentWidth on hover change
 	useEffect(() => {
 		if (ref.current && contentWidth !== 0) {
 			ref.current.style.width = `${contentWidth}px`;
 		}
-	}, [contentWidth])
+	}, [contentWidth]);
 
 	return (
 		<div
 			className={styles.root}
-			onMouseLeave={() => setContent(props.content)}
+			onMouseLeave={() => setContent(props.guess)}
 			onMouseOver={() => {
 				setContent("-");
 			}}
+			onClick={() =>
+				props.IRemoveFromGuesses.removeFromGuesses(
+					props.guess,
+					props.IRemoveFromGuesses.setGuesses,
+					props.IRemoveFromGuesses.remainingReccomendations,
+				)
+			}
 			ref={ref}
 		>
 			{content}
