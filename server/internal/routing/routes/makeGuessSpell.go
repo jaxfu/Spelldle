0113a8861c23325be716169df10e719d817c3d53
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-04 14:48:30 -0400

    makeGuessSpell sends correct

--- server/internal/routing/routes/makeGuessSpell.go
+++ server/internal/routing/routes/makeGuessSpell.go
@@ -13,13 +13,19 @@ type payload struct {
 	SpellID uint `json:"spell_id"`
 }
 
+type response struct {
+	Correct bool `json:"correct"`
+}
+
 func MakeGuessSpell(db *dbHandler.DBHandler) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		response := response{Correct: false}
+
 		// get userID from jwt
 		userID, err := utils.GetJwtInfoFromCtx(ctx)
 		if err != nil {
 			fmt.Printf("error in GetJwtInfoFromCtx %+v\n", err)
-			ctx.Status(http.StatusInternalServerError)
+			ctx.JSON(http.StatusInternalServerError, response)
 			return
 		}
 
@@ -28,7 +34,7 @@ func MakeGuessSpell(db *dbHandler.DBHandler) gin.HandlerFunc {
 		err = ctx.BindJSON(&payload)
 		if err != nil {
 			fmt.Printf("Error binding payload: %v\n", err)
-			ctx.Status(http.StatusInternalServerError)
+			ctx.JSON(http.StatusInternalServerError, response)
 			return
 		}
 		fmt.Printf("spellGuess payload: %+v\n", payload)
@@ -37,24 +43,29 @@ func MakeGuessSpell(db *dbHandler.DBHandler) gin.HandlerFunc {
 		gameSession, err := db.GetGameSessionByUserID(userID)
 		if err != nil {
 			fmt.Printf("Error getting gameSession: %v\n", err)
-			ctx.Status(http.StatusInternalServerError)
+			ctx.JSON(http.StatusInternalServerError, response)
 			return
 		}
 
-		// insert spell
+		// insert spell guess
 		if err := db.InsertGuessSpell(gameSession.GameSessionID, payload.SpellID); err != nil {
 			fmt.Printf("error in InsertGuessSpell: %+v", err)
-			ctx.Status(http.StatusInternalServerError)
+			ctx.JSON(http.StatusInternalServerError, response)
 			return
 		}
 
 		// update gameSession spell_rounds
 		if err := db.UpdateGameSessionSpellRounds(userID, gameSession.SpellRounds+1); err != nil {
 			fmt.Printf("error in UpdateGameSessionSpellRounds: %+v", err)
-			ctx.Status(http.StatusInternalServerError)
+			ctx.JSON(http.StatusInternalServerError, response)
 			return
 		}
 
-		ctx.String(http.StatusOK, "Success")
+		// check if spell is correct
+		if payload.SpellID == gameSession.SpellID {
+			response.Correct = true
+		}
+
+		ctx.JSON(http.StatusOK, response)
 	}
 }
