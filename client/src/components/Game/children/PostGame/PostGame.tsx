98278fb42f05e4b52aa66935dbc9790283ff0a69
Jackson Furr <jackson.a.furr@gmail.com>
2024-09-05 18:37:20 -0400

    new game button

--- client/src/components/Game/children/PostGame/PostGame.tsx
+++ client/src/components/Game/children/PostGame/PostGame.tsx
@@ -3,7 +3,10 @@ import styles from "./PostGame.module.scss";
 const PostGame: React.FC = () => {
 	return (
 		<div className={styles.root}>
-			<div className={styles.content}>PostGame</div>
+			<div className={styles.content}>
+				<h2>PostGame</h2>
+				<button>New Game</button>
+			</div>
 		</div>
 	);
 };
